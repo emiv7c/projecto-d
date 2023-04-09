5c2b525ef0b579616b8df9289369165cf0d0d30f
emi <emivic1858@gmail.com>
2023-04-09 15:22:53 -0300

    se edito la apariencia y se agregaron funciones

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,12 +2,17 @@
 from django.urls import path
 from inicio import views
 
+
+app_name= 'inicio'
+
 urlpatterns = [
-    path('mi_primer_template/', views.mi_primer_template),
-    path('saludar/<nombre>/<apellido>/',views.saludar),
-    path('mostrar_fecha/', views.mostrar_fecha),
+    path('mi_primer_template/', views.mi_primer_template, name='primer_template'),
+    path('saludar/<nombre>/<apellido>/',views.saludar, name='saludar'),
+    path('mostrar_fecha/', views.mostrar_fecha, name= 'fecha'),
     path('', views.vista ),
-    path('mi_prueba_template/', views.mi_prueba_template),
-    path('animal/', views.crear_animal)
+    path('mi_prueba_template/', views.mi_prueba_template,name='prueba_template'),
+    path('animal/', views.crear_animal, name='animal')
    
 ]
+
+
