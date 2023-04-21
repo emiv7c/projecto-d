558a43d4bf7de65d683df45ff9c51bb7e90f7443
emi <emivic1858@gmail.com>
2023-04-20 21:19:36 -0300

    version terminada

--- inicio/views.py
+++ inicio/views.py
@@ -24,8 +24,11 @@ def crear_vehiculo(request):
         formulario = creacionVehiculoFormulario(request.POST)
         if formulario.is_valid():
             datos_formulario = formulario.cleaned_data
-            vehiculo = autos(nombre=datos_formulario['nombre'], nombre_del_auto=datos_formulario['nombre_del_auto'],
-                kilometros_recorridos=datos_formulario['kilometros_recorridos'], detalles_auto=datos_formulario['detalles_auto'])
+            vehiculo = autos(
+                nombre=datos_formulario['nombre'],
+                nombre_del_auto=datos_formulario['nombre_del_auto'],
+                kilometros_recorridos=datos_formulario['kilometros_recorridos'],
+                detalles_auto=datos_formulario['detalles_auto'])
             vehiculo.save()
             return redirect('../../inicio/lista_autos')
     else:
@@ -33,7 +36,5 @@ def crear_vehiculo(request):
     return render(request, 'inicio/crear_vehiculo.html', {'formulario': formulario})
 
 def lista_autos(request):
-    vehiculo=autos.objects.all()
-    return render(request, "inicio/lista_autos.html",{"vehiculo":vehiculo})
-
-    
+    lista_autos = autos.objects.all()
+    return render(request, 'inicio/lista_autos.html', {'lista_autos': lista_autos})
