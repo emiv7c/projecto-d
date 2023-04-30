2c9a9e0eb71e7632da1f0dcedc65cb299d13cd25
emi <emivic1858@gmail.com>
2023-04-30 02:31:14 -0300

    nuevo

--- inicio/urls.py
+++ inicio/urls.py
@@ -7,12 +7,13 @@ app_name= 'inicio'
 
 urlpatterns = [
    # path('about/',views.about, name= 'about'),
-    path('lista_blogs/', views.blogs, name= 'blogs'),
+    #path('lista_blogs/', views.blogs, name= 'blogs'),
     path('inicio/', views.vista, name='inicio'),
     path('crear_blog', views.crear_blog, name='crear-blog'),
-    path('login', views.login_request, name='login'),
-    path('blogs/<int:id_blog>/modificar/', views.modificar_blog, name='modificar_blog'),
+    path('login', views.login, name='login'),
+    path('blogs/<int:PK>/modificar/', views.ModificarBlog.as_view(), name='modificar_blog'),
     path('register', views.registro, name= 'registro'),
+    path('lista_blogs/', views.ListaBlogs.as_view(), name='lista_blog'),
 ]
 
 
--- inicio/views.py
+++ inicio/views.py
@@ -3,7 +3,8 @@ from inicio.models import blog
 from inicio.forms import creacionBlogsFormulario,modificacionDeBlogsFormulario
 from django.contrib.auth import login, authenticate,logout
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
-
+from django.views.generic.edit import CreateView,UpdateView
+from django.views.generic.list import ListView
 
 def login_request(request):
     
@@ -90,20 +91,20 @@ def vista(request):
    # return redirect('inicio:listar_autos')
 
 
-def modificar_blog(request, id_blog):
-    blog_modificar = blog.objects.get(id=id_blog)
-    formulario = modificacionDeBlogsFormulario()  # asignar un valor inicial
-    if request.method == "POST":
-        formulario = modificacionDeBlogsFormulario(request.POST)
-        if formulario.is_valid():
-            data_limpia = formulario.cleaned_data
-            blog_modificar.nombre = data_limpia['nombre']
-            blog_modificar.contenido = data_limpia['contenido']
-            blog_modificar.descripcion_contenido = data_limpia['descripcion_contenido']
-            blog_modificar.save()
-            return redirect('inicio:blogs')
+#def modificar_blog(request, id_blog):
+    #blog_modificar = blog.objects.get(id=id_blog)
+    #formulario = modificacionDeBlogsFormulario()  # asignar un valor inicial
+    #if request.method == "POST":
+        #formulario = modificacionDeBlogsFormulario(request.POST)
+        #if formulario.is_valid():
+            #data_limpia = formulario.cleaned_data
+            #blog_modificar.nombre = data_limpia['nombre']
+            #blog_modificar.contenido = data_limpia['contenido']
+            #blog_modificar.descripcion_contenido = data_limpia['descripcion_contenido']
+            #blog_modificar.save()
+            #return redirect('inicio:blogs')
 
-    return render(request, 'inicio/modificar_blog.html', {'formulario': formulario, 'id_blog': id_blog})
+    #return render(request, 'inicio/modificar_blog.html', {'formulario': formulario, 'id_blog': id_blog})
 
 
 
@@ -127,6 +128,32 @@ def crear_blog(request):
     return render(request, 'inicio/crear_blog.html', {'formulario': formulario})
 
 
-def blogs (request):
-    lista_blogs = blog.objects.all()
-    return render(request, 'inicio/lista_blogs.html', {'lista_blogs': lista_blogs},)
+#def blogs (request):
+    #lista_blogs = blog.objects.all()
+    #return render(request, 'inicio/lista_blogs.html', {'lista_blogs': lista_blogs},)
+
+
+class CrearBlog(CreateView):
+    model: blog
+    template_name = 'inicio/modificar_animal.html'
+    success_url = '/lista_blogs/'
+    fields = ['nombre', 'descripcion', 'contenido']
+    
+
+
+
+
+class ModificarBlog(UpdateView):
+    model: blog
+    template_name = 'inicio/modificar_animal.html'
+    success_url = '/lista_blogs/'
+    fields = ['nombre', 'descripcion', 'contenido']
+    
+    
+class ListaBlogs(ListView):
+    model= blog
+    template_name = 'inicio/CBV/lista_blogs.html'
+        
+    
+    
+    
